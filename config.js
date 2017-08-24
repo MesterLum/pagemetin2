150c7bec2c6f29333e7fb29f26e678b5cbcd215b
cuauhtemoc paes <mesterlum@MacBook-Air-de-cuauhtemoc.local>
2017-08-23 22:53:33 -0600

    Commit backup1

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	//Configuracion de la base de datos
+	DB_HOST :'145.239.169.235',
+	DB_USER : 'root',
+	DB_PASS : 'lara12345',
+	DB_TABLE : 'accounts',
+	DB_PORT : '3306',
+
+	//Configuraciones generales
+
+	PORT : 8080,
+
+	//clave secreta
+
+	SECRET_SESSION : 'claveultrasecretaprroshijosdesusanababitch'
+
+}
